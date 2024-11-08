aa6e049019d811fbc96373247605ca49b33834e6
Aleksandar Marcetic <amarcetic04@gmail.com>
2024-11-08 20:00:49 +0100

    image change

--- constants/projects.js
+++ constants/projects.js
@@ -84,7 +84,7 @@ export const projects = [
     gitHubLink: "https://github.com/CojaDev/Restorani_Subotica2",
     monitorUrl: "https://restorani-subotica.vercel.app/",
     phoneUrl: "https://restorani-subotica.vercel.app/",
-    mobileImg: "/img/websites/restMobile.png",
+    mobileImg: "/img/websites/restmobile.png",
     monitorImg: "/img/websites/resMonitor.webp",
     imageUrl: "/img/websites/res.png",
     onlyMobile: false,
