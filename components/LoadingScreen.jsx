32022b3131e0dcba1798c22392ea3f297def2abf
Aleksandar Marcetic <amarcetic04@gmail.com>
2024-11-13 02:55:24 +0100

    metadata

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -12,7 +12,7 @@ const LoadingScreen = () => {
       setTimeout(() => {
         setShowLoading(false);
       }, 500); // This duration should match the CSS transition duration
-    }, 1200);
+    }, 1000);
   }, []);
 
   useEffect(() => {
